d5fdfc3ea8c91e8ea158aca60ab54188cd119b0a
diana <dianagavrilovich@gmail.com>
2025-07-08 17:57:03 +0300

    User model change. New fields added

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Boolean, DateTime, func
 from src.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -7,5 +8,8 @@ class User(Base):
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     password: Mapped[str] 
     score: Mapped[int] = mapped_column(nullable=False, default=0)
-
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    
     answers = relationship("UserAnswer", back_populates="user", cascade="all, delete-orphan")
